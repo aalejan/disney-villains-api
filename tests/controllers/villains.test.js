f5786bc70d45ae15f5ff1df61192199d10fc4494
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-11-26 14:21:50 -0500

    .sql file created

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -4,6 +4,7 @@ const models = require('../../models')
 const { villainsList, singleVillain } = require('../mocks/villains')
 const sinonChai = require('sinon-chai')
 const { describe, it } = require('mocha')
+const { getAllVillains } = require('../../controllers/villains')
 
 chai.use(sinonChai)
 const { expect } = chai
@@ -13,6 +14,13 @@ describe('Controllers- villains', () => {
   describe('getAllVillains', () => {
     it('retrieves a list of villains from the database and calls response.send() with the list', async () => {
       const stubbedFindAll = sinon.stub(models.villains, 'findAll').returns(villainsList)
+      const stubbedSend = sinon.stub()
+      const response = {send: stubbedSend }
+
+      await getAllVillains({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedSend).to.have.been.calledWith(villainsList)
     })
   })
 })
