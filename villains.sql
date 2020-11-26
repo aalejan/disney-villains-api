9f1d8e26d55d2f0e5cd680fccc6b7fa8856df6b3
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-11-26 14:25:09 -0500

    code to create database added to villains.sql file

---
+++ villains.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE disneyCharacters;
+
+USE disneyCharacters;
+
+CREATE TABLE villains (
+    id INT auto_increment,
+    name VARCHAR(255),
+    movie VARCHAR(255),
+    slug VARCHAR (255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(id)
+)
